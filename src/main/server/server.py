9263e88144c3327bf1aa648e72601d26965c52e8
moises monteiro <moises.monteiro2077@hotmail.com>
2023-11-22 18:03:36 -0300

    Create address_routers

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 
 from src.presentation.middlewares.cnpj_middleware import check_company_cnpj_middleware
-from src.presentation.routers import company_routers, token_routers, user_routers
+from src.presentation.routers import (
+    address_routers,
+    company_routers,
+    token_routers,
+    user_routers,
+)
 
 app = FastAPI()
 
@@ -10,3 +15,4 @@ app.middleware("http")(check_company_cnpj_middleware)
 app.include_router(user_routers.router)
 app.include_router(token_routers.router)
 app.include_router(company_routers.router)
+app.include_router(address_routers.router)
---
+++ src/presentation/routers/address_routers.py
@@ -0,0 +1,104 @@
+# pylint: disable=unused-argument
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, Header, HTTPException, status
+from sqlalchemy.orm import Session
+
+from src.infrastructure.database.settings.db_connection import get_db
+from src.presentation.composers.address.create_address_composer import (
+    create_address_composer,
+)
+from src.presentation.composers.address.delete_address_composer import (
+    delete_address_composer,
+)
+from src.presentation.composers.address.get_address_composer import get_address_composer
+from src.presentation.composers.address.list_addresses_composer import (
+    list_addresses_composer,
+)
+from src.presentation.composers.address.update_address_composer import (
+    update_address_composer,
+)
+from src.presentation.dependencies.get_current_user import get_current_user
+from src.presentation.schemas.address import AddressCreate, AddressOut, AddressUpdate
+from src.presentation.schemas.user import UserOut
+
+router = APIRouter(prefix="/addresses", tags=["addresses"])
+
+
+@router.get("/{id}", status_code=status.HTTP_200_OK, response_model=AddressOut)
+def get_address(
+    id: int,
+    x_company_cnpj: Annotated[str, Header()],
+    current_user: UserOut = Depends(get_current_user),
+    session: Session = Depends(get_db),
+):
+    address = get_address_composer(session, id)
+    if address:
+        return address
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail="get-address for id, not found."
+    )
+
+
+@router.get("/", status_code=status.HTTP_200_OK, response_model=list[AddressOut])
+def list_addresses(
+    x_company_cnpj: Annotated[str, Header()],
+    current_user: UserOut = Depends(get_current_user),
+    session: Session = Depends(get_db),
+):
+    address = list_addresses_composer(current_user.user_id, session)
+    if address:
+        return address
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail="List addresses not found."
+    )
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=AddressOut)
+def create_address(
+    address: AddressCreate,
+    x_company_cnpj: Annotated[str, Header()],
+    current_user: UserOut = Depends(get_current_user),
+    session: Session = Depends(get_db),
+):
+    address = create_address_composer(session, address, current_user.id)
+    if address:
+        return address
+    raise HTTPException(
+        status_code=status.HTTP_409_CONFLICT,
+        detail="Address already exists or not found.",
+    )
+
+
+@router.patch("/update/{id}", status_code=status.HTTP_200_OK, response_model=AddressOut)
+def update_address(
+    id: int,
+    address: AddressUpdate,
+    x_company_cnpj: Annotated[str, Header()],
+    current_user: UserOut = Depends(get_current_user),
+    session: Session = Depends(get_db),
+):
+    # user  = current_user.id
+    address = address.model_dump(exclude_unset=True)
+    address = update_address_composer(session, id, address)
+    if address:
+        return address
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail="Update Address, not found."
+    )
+
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_address(
+    id: int,
+    x_company_cnpj: Annotated[str, Header()],
+    current_user: UserOut = Depends(get_current_user),
+    session: Session = Depends(get_db),
+):
+    # user  = current_user.id
+    address = delete_address_composer(session, id)
+    if address:
+        return
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail="Delete address not found."
+    )
