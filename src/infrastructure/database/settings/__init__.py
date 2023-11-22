74fd7665f5792073f7dff34686326fe39d570924
moises monteiro <moises.monteiro2077@hotmail.com>
2023-11-22 17:48:24 -0300

    Create address_init

--- src/infrastructure/database/settings/__init__.py
+++ src/infrastructure/database/settings/__init__.py
@@ -1,3 +1,4 @@
+from src.infrastructure.database.models.address import Address
 from src.infrastructure.database.models.company import Company
 from src.infrastructure.database.models.user import User
 from src.infrastructure.database.models.user_company import UserCompany
