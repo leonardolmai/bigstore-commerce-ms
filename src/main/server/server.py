7d56d2070da7ebf24a40adcb55a2417ae5bef43e
moises monteiro <moises.monteiro2077@hotmail.com>
2023-11-22 18:39:04 -0300

    Create card_main_router

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from src.presentation.middlewares.cnpj_middleware import check_company_cnpj_middleware
 from src.presentation.routers import (
     address_routers,
+    card_routers,
     company_routers,
     token_routers,
     user_routers,
@@ -16,3 +17,4 @@ app.include_router(user_routers.router)
 app.include_router(token_routers.router)
 app.include_router(company_routers.router)
 app.include_router(address_routers.router)
+app.include_router(card_routers.router)
