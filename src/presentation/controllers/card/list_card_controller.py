f929b93e9d4771efd1afffc8d6bab143776e2262
moises monteiro <moises.monteiro2077@hotmail.com>
2023-11-22 18:35:48 -0300

    Create card_controller

---
+++ src/presentation/controllers/card/list_card_controller.py
@@ -0,0 +1,11 @@
+from src.domain.use_cases.card.list_card import ListCardUseCaseInterface
+from src.presentation.schemas.card import CardOut
+
+
+class ListCardController:
+    def __init__(self, use_case: ListCardUseCaseInterface) -> None:
+        self.__use_case = use_case
+
+    def handle(self, user_id: int) -> list[CardOut] | None:
+        response = self.__use_case.execute(user_id)
+        return response
