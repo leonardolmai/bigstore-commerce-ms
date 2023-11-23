9a16f0efec84a06b93473f6ae200729ccf28a94d
moises monteiro <moises.monteiro2077@hotmail.com>
2023-11-23 16:08:06 -0300

    Update factories

--- tests/factories/factories.py
+++ tests/factories/factories.py
@@ -2,6 +2,8 @@ import factory
 from factory import Faker
 from sqlalchemy import orm
 
+from src.infrastructure.database.models.address import Address
+from src.infrastructure.database.models.card import Card
 from src.infrastructure.database.models.company import Company
 from src.infrastructure.database.models.user import User
 
@@ -32,3 +34,30 @@ class CompanyFactory(factory.alchemy.SQLAlchemyModelFactory):
     owner = factory.SubFactory(UserFactory)
     is_approved = Faker("boolean")
     is_active = Faker("boolean")
+
+
+class AddressFactory(factory.alchemy.SQLAlchemyModelFactory):
+    class Meta:
+        model = Address
+        sqlalchemy_session = Session
+
+    name = Faker("name")
+    postal_code = Faker("postcode")
+    uf = Faker("state_abbr")
+    city = Faker("city")
+    neighborhood = Faker("city_suffix")
+    street = Faker("street_name")
+    number = Faker("building_number")
+    complement = Faker("secondary_address")
+
+
+class CardFactory(factory.alchemy.SQLAlchemyModelFactory):
+    class Meta:
+        model = Card
+        sqlalchemy_session = Session
+
+    name = Faker("name")
+    card_number = Faker("credit_card_number")
+    expiration_month = Faker("month")
+    expiration_year = Faker("year")
+    cvc = Faker("credit_card_security_code")
