6c9f00b42c22dcab551580cc5dbeeb15ec0f5f07
Leonardo Lima <se.leonardolima@gmail.com>
2023-11-24 18:20:50 -0300

    Add product category enum

---
+++ src/infrastructure/database/models/enums/product_category.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class ProductCategory(Enum):
+    ELECTRONICS = "electronics"
+    CLOTHING = "clothing"
+    HOME = "home"
+    HEALTH_BEAUTY = "health_beauty"
+    SPORTS_OUTDOORS = "sports_outdoors"
+    BOOKS_MOVIES_MUSIC = "books_movies_music"
+    TOYS_GAMES = "toys_games"
+    AUTOMOTIVE = "automotive"
+    FURNITURE_HOME_DECOR = "furniture_home_decor"
+    OFFICE_SUPPLIES = "office_supplies"
