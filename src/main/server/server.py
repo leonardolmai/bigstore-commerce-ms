a5d79b5060320712bf954ac1ae30d175aaccc674
Leonardo Lima <se.leonardolima@gmail.com>
2023-12-11 08:51:14 -0300

    Mount static file directory

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from src.presentation.middlewares.cnpj_middleware import check_company_cnpj_middleware
 from src.presentation.routers import (
@@ -13,6 +14,9 @@ from src.presentation.routers import (
 app = FastAPI()
 
 app.middleware("http")(check_company_cnpj_middleware)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 app.include_router(user_routers.router)
 app.include_router(token_routers.router)
