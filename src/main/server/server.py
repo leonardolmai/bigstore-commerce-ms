6ee9cbbb02775a1501dd035ddc82bbebfec8c514
Leonardo Lima <se.leonardolima@gmail.com>
2023-12-31 16:35:49 -0300

    Add cors middleware

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
 from src.presentation.middlewares.cnpj_middleware import check_company_cnpj_middleware
@@ -14,10 +15,23 @@ from src.presentation.routers import (
 
 app = FastAPI()
 
+
 app.middleware("http")(check_company_cnpj_middleware)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+origins = ["http://localhost:3000", "http://0.0.0.0:8000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    # allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    # allow_methods=["POST", "GET", "OPTIONS"],
+    # allow_headers=["Content-Type", "Authorization"],
+)
 
 app.include_router(user_routers.router)
 app.include_router(token_routers.router)
